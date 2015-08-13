e6cc9dc435721956e9d0523e4e80dce78c425b2e
Chris Garrett <chris@chrisg.com>
2015-08-13 13:33:18 -0600

    Update dr_who_arduino_SD_wav_audio.ino
    Updated with pinouts and LED light effect

--- dr_who_arduino_SD_wav_audio/dr_who_arduino_SD_wav_audio.ino
+++ dr_who_arduino_SD_wav_audio/dr_who_arduino_SD_wav_audio.ino
@@ -1,11 +1,32 @@
-// Include SPI library
-#include <SPI.h>
+/*
 
+DOCTOR WHO TARDIS WITH 
+SD CARD SOUNDS AND LIGHT EFFECT
+
+Chris Garrett
+Twitter: @makerhacks @chrisgarrett
+HTTP://MAKERHACKS.COM
+
+
+pro mini
+13 orange slk
+12 purple miso
+11 yellow mosi
+10 blue cs
+
+pro micro
+15 orange slk
+14 purple miso
+16 yellow mosi
+10 blue cs
+
+
+*/
 
 // Include the SD library
 #include <SD.h>
 
-// use SD Card pin 
+// use pin 
 #define SD_CARD 10  
 
 // This library plays the audio
@@ -14,9 +35,17 @@
 // create an audio object
 TMRpcm sample;   
 
+	// LED pin
+	int led = 3; 			// the pin that the LED is attached to
+	int brightness = 0;    // how bright the LED is
+	int fadeAmount = 5;    // how many points to fade the LED by
 
 // runs on startup 
-void setup(){
+void setup()
+{
+
+  // LED
+  pinMode(led, OUTPUT); 
 
   // randomise
   randomSeed(analogRead(0));
@@ -31,32 +60,54 @@ void setup(){
   }
 
  // quite loud
- sample.volume(6);
+ sample.volume(9);
  
  // play the start up sound
  sample.play("theme.wav"); 
  
-
 }
  
-void loop(){  
+void loop()
+{  
 
-	// wait until the current sound ends	
+  	// wait until the current sound ends	
 	while ( sample.isPlaying() )
 	{
-		// do nothing
-	}
- 
+          // set the brightness of pin :
+          analogWrite(led, brightness);    
+        
+          // change the brightness for next time through the loop:
+          brightness = brightness + fadeAmount;
+        
+          // reverse the direction of the fading at the ends of the fade: 
+          if (brightness == 0 || brightness == 255) 
+          {
+            fadeAmount = -fadeAmount ; 
+          }     
+          // wait for 30 milliseconds to see the dimming effect    
+          delay(30);                            
+        }
+
+	// stopped so play another
+	ran();
+
+
+}
+
+// Currently just picks random
+// sound to play
+void ran()
+{
  	// pick a random number
  	int randNumber = random(12);
  
  // play the randomly picked sound	
  switch (randNumber) {
     case 1:
-      //sample.play("delete.wav");
+      sample.play("delete.wav");
       break;
     case 2:
-      //sample.play("deleted.wav");
+      sample.play("deleted.wav");
       break;
 	case 3:   
 	  sample.play("emergency.wav");
